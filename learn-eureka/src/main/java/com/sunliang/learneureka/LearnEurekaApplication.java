355ae7c217ba3fba9914b1a20c5dc6e1a0a67f15
15701362@qq.com <15701362@qq.com>
2019-11-25 21:03:37 +0800

    创建eureka，两个application.properties 设置了不同的端口，实际情况下应该由两个真实的实例。

---
+++ learn-eureka/src/main/java/com/sunliang/learneureka/LearnEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.sunliang.learneureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class LearnEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnEurekaApplication.class, args);
+    }
+
+}
---
+++ learn-eureka/src/test/java/com/sunliang/learneureka/LearnEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunliang.learneureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
